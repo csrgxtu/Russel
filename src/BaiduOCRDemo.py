298e6e1ec9c5e9c59ebb1217b256beb1091dd6ae
archer <1246506786@qq.com>
2016-09-14 11:24:43 +0800

    都失效了

--- src/BaiduOCRDemo.py
+++ src/BaiduOCRDemo.py
@@ -15,7 +15,7 @@ data['detecttype'] = "LocateRecognize"
 data['languagetype'] = "CHN_ENG"
 data['imagetype'] = "1"
 
-file_object = open('../data/ffebb7f8-0a6c-4380-b4b8-ee8b4b8ae7a2.jpg_5.jpg','rb')
+file_object = open('../data/1453734015286_spine.jpg','rb')
 try:
      tmp = file_object.read( )
 finally:
@@ -27,16 +27,17 @@ decoded_data = urllib.urlencode(data)
 req = urllib2.Request(url, data = decoded_data)
 
 req.add_header("Content-Type", "application/x-www-form-urlencoded")
-req.add_header("apikey", "1466dccc8ec8e42feaf18f2e05c543fc")
+req.add_header("apikey", "150281dc441994b2d21ddb0e57a9bd48")
 
 resp = urllib2.urlopen(req)
 content = resp.read()
+print content
 if(content):
     # print(content)
     title = ast.literal_eval(content)['retData'][0]['word']
-    print title
+    print unicode(title, 'utf-8')
     # print type(title), type(unicode(title, 'utf-8'))
     # print unicode(title, 'utf-8').encode('utf-8', 'ignore')
     # print u'\u4e2d\u56fd\u4eba\u7684\u670d\u9970\u548c\u4e60\u4fd7\u56fe\u9274'
     # appendstr2fileutf8(u'\u4e2d\u56fd\u4eba\u7684\u670d\u9970\u548c\u4e60\u4fd7\u56fe\u9274', './result.csv')
-    appendstr2fileutf8(title, './result.csv')
+    # appendstr2fileutf8(title, './result.csv')
